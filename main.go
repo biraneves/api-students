c2a6c1c1b03b101eff991b1579eca2474095131b
Ubirajara Neves <ubirajara.neves@usp.br>
2024-08-06 18:16:08 -0300

    Add students routes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -17,6 +18,10 @@ func main() {
 
 	// Routes
 	e.GET("/students", getStudents)
+	e.POST("/students", createStudent)
+	e.GET("/students/:id", getStudent)
+	e.PUT("/students/:id", updateStudent)
+	e.DELETE("/students/:id", deleteStudent)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
@@ -25,4 +30,26 @@ func main() {
 // Handlers
 func getStudents(c echo.Context) error {
 	return c.String(http.StatusOK, "List with all students")
+}
+
+func createStudent(c echo.Context) error {
+	return c.String(http.StatusOK, "Create a student")
+}
+
+func getStudent(c echo.Context) error {
+	id := c.Param("id")
+	strMsg := fmt.Sprintf("Info from student %s", id)
+	return c.String(http.StatusOK, strMsg)
+}
+
+func updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	strMsg := fmt.Sprintf("Update infos of student %s", id)
+	return c.String(http.StatusOK, strMsg)
+}
+
+func deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	strMsg := fmt.Sprintf("Delete student %s", id)
+	return c.String(http.StatusOK, strMsg)
 }
